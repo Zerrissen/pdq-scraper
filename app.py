6c88d722475568471837b6bd42540622c150d117
Zerrissen <54736633+Zerrissen@users.noreply.github.com>
2023-05-23 22:18:17 +1200

    HTTP Requests Changes to be committed: 	modified:   app.py 	modified:   package-lock.json 	modified:   static/dist/css/tailwind.css 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/scrape', methods=['POST', 'GET'])
+def scrape():
+    if request.method == 'GET':
+        return index()
+    else:
+        api_key = request.form['shodan-api-key']
+        return render_template('index.html', api_key=api_key)
+
+def handle_data():
+    return json.dumps({'api_key': request.form['shodan-api-key-input']})
 
 if __name__ == '__main__':
     app.run(debug=True)
